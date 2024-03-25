83827adbfb4c2985bcdf26cef8d648fbd8dea699
Daniel Roberts <daniel.roberts@Cadre5.com>
2024-03-25 15:22:09 -0400

    Added responsive styling

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function completeAction() {
     alert("No internet connection found. Please try again.");
 }
+
+function toggleNav() {
+    document.getElementById("navDropdown").classList.toggle("shown");
+}
